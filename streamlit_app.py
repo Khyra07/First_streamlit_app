aca80e1914521c09227cf3dc3d4252a19696fa65
Khyra07 <129060543+Khyra07@users.noreply.github.com>
2023-03-28 22:32:20 +0800

    Update streamlit_app.py
    check to Confirm the Snowflake Connector Package

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,3 +32,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # to show the output in the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
